c124b102c6148dc558885cdd92030a8cb93fd937
unknown <vargasalejandro2506o@gmail.com>
2022-02-12 10:46:42 -0600

    page updated

--- scripts/hamburger-btn.js
+++ scripts/hamburger-btn.js
@@ -1,10 +1,10 @@
-const hamburgerBttn = document.getElementById("hamburger"), 
+const $hambugerBtn = document.getElementById("hamburger"), 
 menuMobile = document.getElementById("menu-mobile"),
 overlay = document.querySelector(".overlay");
 
 document.addEventListener("click", e => {
-    if (e.target === hamburgerBttn ||
-        e.target.matches("#hamburger *") ||
+    console.log(e.target);
+    if (e.target === $hambugerBtn ||
         e.target.matches(".mobile-item")) {
         menuMobile.classList.toggle("display");
         document.body.classList.toggle("overflowYHidden");
